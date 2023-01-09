602d3eb8a64adeba1dc757ac8221390a81563799
xty <xty1821901@163.com>
2023-01-09 16:53:11 +0800

    Complete project structure

--- backend/src/main/java/com/kob/backend/controller/pk/BotInfoController.java
+++ backend/src/main/java/com/kob/backend/controller/pk/BotInfoController.java
@@ -1,18 +1,25 @@
 package com.kob.backend.controller.pk;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/pk/")
 public class BotInfoController {
-    @RequestMapping("getBotInfo/")
-    public List<String> getInfo(){
-        List list = new ArrayList();
-        list.addAll(List.of("shit","bitch","motherfuker"));
-        return list;
+    @RequestMapping("/getBotInfo/")
+    @CrossOrigin
+    public Map<String,String> getInfo(){
+        Map map = new HashMap();
+        map.put("name","xty");
+        map.put("rating","2500");
+        map.put("name","yxc");
+        map.put("rating","2400");
+        return map;
     }
 }
