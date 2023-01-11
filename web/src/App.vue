76e81741ec296a60f1895ee5214945442652dcf3
xty <xty1821901@163.com>
2023-01-11 19:25:58 +0800

    Web: add navigation bar and text box

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,36 +1,16 @@
 <template>
-  <div>
-    <div>A昵称: {{ bot_name }}</div>
-    <div>A战力: {{ bot_rating }}</div>
-  </div>
+  <NavBar />
   <router-view></router-view>
 </template>
 
 <script>
-import $ from 'jquery';
-import { ref } from 'vue';
+import NavBar from '@/components/NavBar.vue'
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/js/bootstrap"
 
 export default{
-  name: "App",
-  setup: () => {
-    let bot_name = ref("");
-    let bot_rating = ref("");
-
-
-    $.ajax({
-      url: "http://127.0.0.1:3000/pk/getBotInfo/",
-      type: "get",
-      success: resp => {
-        // console.log(resp);
-        bot_name.value = resp.name;
-        bot_rating.value = resp.rating;
-      }
-    });
-
-    return {
-      bot_name,
-      bot_rating
-    }
+  components: {
+    NavBar
   }
 }
 </script>
---
+++ web/src/components/ContentField.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="container content-field">
+        <div class="card">
+            <div class="card-body">
+                <slot></slot>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import { computed } from 'vue';
+import { useRoute} from 'vue-router';
+
+export default {
+    setup() {
+        const route = useRoute();
+        let route_name = computed(() => route.name)
+        return {
+            route_name
+        }
+    }
+}
+</script>
+
+<style scoped>
+div.content-field {
+    margin-top: 20px;
+}
+</style>
